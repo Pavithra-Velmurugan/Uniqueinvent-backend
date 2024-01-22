f379bfc656f9722d348f0bf466245afe498d5d0a
pavithra-velmurugan <pavithravelmurugan1497@gmail.com>
2024-01-22 12:46:58 +0530

    changes done

--- model/billModel.js
+++ model/billModel.js
@@ -4,11 +4,11 @@ const mongoose = require ('mongoose')
 const billSchema = mongoose.Schema(
   {
   customerName:{type:String, required: true},
-customerPhoneNumber:{type:Number, required: true},
-  totalAmount:{type:Number, required: true},
+customerPhoneNumber:{type:String, required: true},
+  totalAmount:{type:String, required: true},
   //calculate tax => 2% => (2*totalAmount)/100= 0.9
-  tax:{type:Number, required: true},
-  subTotal:{type:Number, required: true},
+  tax:{type:String, required: true},
+  subTotal:{type:String, required: true},
   paymentMode:{type:String, required: true},
   cartItems:{type:Array, required: true},
 },
